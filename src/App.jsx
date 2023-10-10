53dd4665a5999a3351befdea4041d741640739ef
nelio <neliobino@gmail.com>
2023-10-10 14:00:09 +0200

    minhas primeiras tentativas

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import FormLogin from "./assets/FormLogIn/FormLogin"
+export default function App() {
+  return (
+    <div className="container">
+      <FormLogin/>
+    </div>
+  )
+}
---
+++ src/assets/FormLogIn/FormLogin.jsx
@@ -0,0 +1,7 @@
+export default ()=>{
+    return(
+        <form className="p-5 border rounded shadow" action="">
+            <a href="index.jtml">clik</a>
+        </form>
+    )
+}
